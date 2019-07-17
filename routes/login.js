dfdfbab899eb67d7520a93787d4378807cf66b78
enis.keskin <eniskeskin61@hotmail.com>
2019-07-17 15:51:53 +0300

    create register

--- routes/login.js
+++ routes/login.js
@@ -1,19 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
 
-const User = require('../models/User');
-
-router.post('/register', (req, res, next) => {
-  const Register = new User(req.body);
-  const promise = Register.save();
-
-  promise.then((data) => {
-    res.json({ status: 1 });
-  }).catch((err) => {
-    res.json(err);
-  });
-})
 
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Login Page' });
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
+const User = require('../models/User');
 
+router.post('/register', (req, res, next) => {
+  const Register = new User(req.body);
+  const promise = Register.save();
+
+  promise.then((data) => {
+    res.json({ status: 1 });
+  }).catch((err) => {
+    res.json(err);
+  });
+})
 
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
