11349cde395f81a62fc7cc9b77696479b9a83563
Emre Can HACIOSMANOĞLU <emre.can.haciosmanoglu@hotmail.com>
2019-07-17 15:21:21 +0300

    small fix

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/users', usersRouter);
 app.use('/addQuestion', addQuestionRouter);
 app.use('/addQuiz', addQuizRouter);
 app.use('/answer', answersRouter);
