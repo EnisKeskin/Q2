8675941862341eff073d859aabc611801b339770
enis.keskin <eniskeskin61@hotmail.com>
2019-07-18 10:46:50 +0300

    delete addquestion and login routes && create Quiz model

--- routes/addquiz.js
+++ routes/addquiz.js
@@ -2,8 +2,22 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 
-router.get('/', function(req, res, next) {
+const Quiz = require('../models/Quiz');
+
+router.get('/', (req, res, next) => {
   res.send('Add a quiz!');
 });
+
+router.post('/', (req, res) => {
+  const quiz = new Quiz(req.body);
+  const promise = quiz.save();
+
+  promise.then((data) => {
+    res.json({ status: 1 })
+  }).catch((err) => {
+    res.json({ err })
+  })
+
+})
 
 module.exports = router;
