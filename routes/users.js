5a2143cbb9c3bafa9dbb177bd6eb0e50463c2785
enis.keskin <eniskeskin61@hotmail.com>
2019-07-18 09:00:43 +0300

    user login created post

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,62 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
+//Models
 const User = require('../models/User');
 
 router.post('/register', (req, res, next) => {
-  const Register = new User(req.body);
-  const promise = Register.save();
+  const { email, password, firstname, lastname, username } = req.body;
+  bcrypt.hash(password, 10).then((hash) => {
+    const user = new User({
+      email,
+      firstname,
+      lastname,
+      username,
+      password: hash
+    });
+    const promise = user.save();
 
-  promise.then((data) => {
-    res.json({ status: 1 });
-  }).catch((err) => {
-    res.json(err);
+    promise.then((data) => {
+      res.json({ status: 1 });
+    }).catch((err) => {
+      res.json(err);
+    });
+  })
+});
+
+
+router.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  
+  User.findOne({
+    email
+  }, (err, user) => {
+    if (err)
+      throw err
+    if (!user) {
+      res.json({
+        status: false,
+        message: 'Authenticaton failed, user not found.'
+      });
+    } else {
+      bcrypt.compare(password, user.password).then((result) => {
+        if (!result) {
+          //şifrenin eşleşmeme durumu
+          res.render('index', {
+            status: 0,
+            message: "Login error",
+          });
+        } else {
+          res.redirect('/users');
+        }
+      });
+    };
   });
-})
+});
 
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.send('respond with a resource');
 });
 
