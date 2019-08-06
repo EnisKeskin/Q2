63faa41f88b930eb417fd043c524a4116a28c96d
Emre Can HACIOSMANOĞLU <emre.can.haciosmanoglu@hotmail.com>
2019-08-06 09:34:01 +0300

    discover page update

--- routes/discover.js
+++ routes/discover.js
@@ -1,8 +1,61 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('discover');
+router.get('/', function (req, res, next) {
+  res.render('discover', {
+    Trending: [
+      {
+        slickIndex: 0,
+        quizImage: "images/sago.jpg",
+        quizTitle: "Quiz",
+        questionCount: 5,
+        ownerName: "Burak",
+        ownerImage: "images/sago.jpg"
+      },
+      {
+        slickIndex: 1,
+        quizImage: "images/sago.jpg",
+        quizTitle: "Quiz",
+        questionCount: 5,
+        ownerName: "Burak",
+        ownerImage: "images/sago.jpg"
+      },
+      {
+        slickIndex: 2,
+        quizImage: "images/sago.jpg",
+        quizTitle: "Quiz",
+        questionCount: 5,
+        ownerName: "Burak",
+        ownerImage: "images/sago.jpg"
+      }
+    ],
+    Discover: [
+      {
+        slickIndex: 0,
+        quizImage: "images/sago.jpg",
+        quizTitle: "Quiz",
+        questionCount: 5,
+        ownerName: "Burak",
+        ownerImage: "images/sago.jpg"
+      },
+      {
+        slickIndex: 1,
+        quizImage: "images/sago.jpg",
+        quizTitle: "Quiz",
+        questionCount: 5,
+        ownerName: "Burak",
+        ownerImage: "images/sago.jpg"
+      },
+      {
+        slickIndex: 2,
+        quizImage: "images/sago.jpg",
+        quizTitle: "Quiz",
+        questionCount: 5,
+        ownerName: "Burak",
+        ownerImage: "images/sago.jpg"
+      }
+    ]
+  });
 });
 
 module.exports = router;
