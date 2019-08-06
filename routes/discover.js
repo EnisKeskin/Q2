710669a954a04c91f04ef0f716d341886930cfbb
Emre Can HACIOSMANOĞLU <emre.can.haciosmanoglu@hotmail.com>
2019-08-06 11:24:04 +0300

    save

--- routes/discover.js
+++ routes/discover.js
@@ -1,75 +1,100 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const quiz = require('../models/Quiz');
 
 router.get('/', function (req, res, next) {
-  res.render('discover', {
-    Trending: [
-      {
-        quizImage: "images/sago.jpg",
-        quizTitle: "Quiz",
-        questionCount: 5,
-        ownerName: "Burak",
-        ownerImage: "images/sago.jpg"
-      },
-      {
-        quizImage: "images/sago.jpg",
-        quizTitle: "Quiz",
-        questionCount: 5,
-        ownerName: "Burak",
-        ownerImage: "images/sago.jpg"
-      },
-      {
-        quizImage: "images/sago.jpg",
-        quizTitle: "Quiz",
-        questionCount: 5,
-        ownerName: "Burak",
-        ownerImage: "images/sago.jpg"
-      },
-      {
-        quizImage: "images/sago.jpg",
-        quizTitle: "Quiz",
-        questionCount: 5,
-        ownerName: "Burak",
-        ownerImage: "images/sago.jpg"
-      }
-    ],
-    Discover: [
-      {
-        quizImage: "images/sago.jpg",
-        quizTitle: "Quiz",
-        questionCount: 5,
-        ownerName: "Burak",
-        ownerImage: "images/sago.jpg"
-      },
-      {
-        quizImage: "images/sago.jpg",
-        quizTitle: "Quiz",
-        questionCount: 5,
-        ownerName: "Burak",
-        ownerImage: "images/sago.jpg"
-      },
-      {
-        quizImage: "images/sago.jpg",
-        quizTitle: "Quiz",
-        questionCount: 5,
-        ownerName: "Burak",
-        ownerImage: "images/sago.jpg"
-      },
-      {
-        quizImage: "images/sago.jpg",
-        quizTitle: "Quiz",
-        questionCount: 5,
-        ownerName: "Burak",
-        ownerImage: "images/sago.jpg"
-      },
-      {
-        quizImage: "images/sago.jpg",
-        quizTitle: "Quiz",
-        questionCount: 5,
-        ownerName: "Burak",
-        ownerImage: "images/sago.jpg"
-      }
-    ]
+
+  quiz.find({}).then((data) => {
+    var quizzes = [];
+    
+    if (data.length > 0) {
+      data.forEach((quiz) => {
+        quizzes.push({
+          quizImage: quiz.img,
+          quizTitle: quiz.title,
+          questionCount: quiz.question.length,
+          ownerName: "Burak",
+          ownerImage: "images/sago.jpg"
+        })
+      });
+
+      res.render('discover',
+        {
+          Trending: quizzes,
+          Discover: quizzes
+        })
+    } else {
+      res.render('discover', {
+        Trending: [
+          {
+            quizImage: "images/sago.jpg",
+            quizTitle: "Quiz",
+            questionCount: 5,
+            ownerName: "Burak",
+            ownerImage: "images/sago.jpg"
+          },
+          {
+            quizImage: "images/sago.jpg",
+            quizTitle: "Quiz",
+            questionCount: 5,
+            ownerName: "Burak",
+            ownerImage: "images/sago.jpg"
+          },
+          {
+            quizImage: "images/sago.jpg",
+            quizTitle: "Quiz",
+            questionCount: 5,
+            ownerName: "Burak",
+            ownerImage: "images/sago.jpg"
+          },
+          {
+            quizImage: "images/sago.jpg",
+            quizTitle: "Quiz",
+            questionCount: 5,
+            ownerName: "Burak",
+            ownerImage: "images/sago.jpg"
+          }
+        ],
+        Discover: [
+          {
+            quizImage: "images/sago.jpg",
+            quizTitle: "Quiz",
+            questionCount: 5,
+            ownerName: "Burak",
+            ownerImage: "images/sago.jpg"
+          },
+          {
+            quizImage: "images/sago.jpg",
+            quizTitle: "Quiz",
+            questionCount: 5,
+            ownerName: "Burak",
+            ownerImage: "images/sago.jpg"
+          },
+          {
+            quizImage: "images/sago.jpg",
+            quizTitle: "Quiz",
+            questionCount: 5,
+            ownerName: "Burak",
+            ownerImage: "images/sago.jpg"
+          },
+          {
+            quizImage: "images/sago.jpg",
+            quizTitle: "Quiz",
+            questionCount: 5,
+            ownerName: "Burak",
+            ownerImage: "images/sago.jpg"
+          },
+          {
+            quizImage: "images/sago.jpg",
+            quizTitle: "Quiz",
+            questionCount: 5,
+            ownerName: "Burak",
+            ownerImage: "images/sago.jpg"
+          }
+        ]
+      });
+    }
   });
 });
 
