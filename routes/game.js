c7fa030aee14f5011ee0339c1b6a5be0ee11576c
Emre Can HACIOSMANOĞLU <emre.can.haciosmanoglu@hotmail.com>
2019-08-23 09:29:52 +0300

    refactoring the game.js

--- routes/game.js
+++ routes/game.js
@@ -3,38 +3,42 @@ module.exports = (io) => {
   const router = express.Router();
   const gameNamespace = io.of("/game");
   const mongoose = require('mongoose');
-  const quiz = require('../models/Quiz');
+  const Quiz = require('../models/Quiz');
   const Player = require('../models/Player');
   const Answer = require('../models/Answer');
   const SECOND = 1000;
   const STATISTICS_TIMEOUT = 4 * SECOND;
+
   router.get('/', (req, res, next) => {
 
     const pin = req.query.pin;
     var Room = Rooms[pin];
     if (Room != null || Room != undefined) {
-      res.render('game', { value: 0, time: 100 });
+      return res.render('game', { value: 0, time: 100 });
     }
     else {
-      quiz.find({ pin: pin }).then((data) => {
-        if (data.length != 0) {
+      Quiz.findOne({ pin: pin }, (error, quiz) => {
+        if (error) {
+          console.error(error);
+          return res.redirect('/');
+        }
+        else if (!quiz) {
+          alert("There is no game with given pin");
+          return res.redirect("/");
+        }
+        else {
           Rooms[pin] = {
             players: {},
+            quiz: quiz,
             started: false,
             questionIndex: 0,
-            questionCount: data[0].question.length,
+            questionCount: quiz.question.length,
             playersAnswered: 0,
             answers: [0, 0, 0, 0],
             time: 0
           };
           res.render('game', { value: 0, time: 100 });
-        } else {
-          alert("There is no game with given pin");
-          res.redirect("/");
         }
-      }).catch((err) => {
-        alert('error: ' + err);
-        res.redirect("/");
       });
     }
   });
@@ -44,103 +48,96 @@ module.exports = (io) => {
 
     socket.on('enter-lobby', (pin) => {
       var Room = Rooms[pin];
-      if (1)
-        var thePlayer = new Player('player_' + Math.floor(Math.random() * 10000), socket);
+      var quiz = Room.quiz;
+      var questions = quiz.questions;
+      var thePlayer = new Player('player_' + Math.floor(Math.random() * 10000), socket);
       Room.players[socket.id] = thePlayer;
-      quiz.find({ pin: pin }).then((data) => {
-        if (data.length != 0) {
-          if (Room.started) {
-            const index = Room.questionIndex;
-            var question = data[0].question[index];
+      if (Room.started) {
+        const index = Room.questionIndex;
+        const question = questions[index];
+        thePlayer.fillAnswers(questions.length);
 
-            thePlayer.fillAnswers(data[0].question.length);
+        gameNamespace.to(socket.id).emit('render-content',
+          {
+            source: handleBarsSources["QH"],
+            templatedata: {
+              questionImage: question.img,
+              question: question.questionTitle,
+              answer1: question.answers[0],
+              answer2: question.answers[1],
+              answer3: question.answers[2],
+              answer4: question.answers[3],
+              value: 100,
+              time: question.time
+            }
+          });
+      }
+      else {
+        var playersInLobby = [];
+        const players = Object.values(Room.players);
+        thePlayer.fillAnswers(questions.length);
 
-            gameNamespace.to(socket.id).emit('render-content',
-              {
-                source: handleBarsSources["QH"],
-                templatedata: {
-                  questionImage: question.img,
-                  question: question.questionTitle,
-                  answer1: question.answers[0],
-                  answer2: question.answers[1],
-                  answer3: question.answers[2],
-                  answer4: question.answers[3],
-                  value: 100,
-                  time: question.time
-                }
-              });
-          }
-          else {
-            var playersInLobby = [];
-            const players = Object.values(Room.players);
+        players.forEach((player) => {
+          playersInLobby.push({ name: player.username });
+        });
 
-            thePlayer.fillAnswers(data[0].question.length);
-
-            players.forEach((player) => {
-              playersInLobby.push({ name: player.username });
+        players.forEach((player) => {
+          gameNamespace.to(player.socket.id).emit('render-content',
+            {
+              source: handleBarsSources["GH"],
+              templatedata: {
+                pin: pin,
+                players: playersInLobby,
+                num: playersInLobby.length
+              }
             });
-            players.forEach((player) => {
-              gameNamespace.to(player.socket.id).emit('render-content',
-                {
-                  source: handleBarsSources["GH"],
-                  templatedata: {
-                    pin: pin,
-                    players: playersInLobby,
-                    num: playersInLobby.length
-                  }
-                });
-            });
-          }
-        }
-      });
+        });
+      }
     });
 
     socket.on('start-the-game', (data) => {
-      var pin = data.pin;
+      const pin = data.pin;
       var Room = Rooms[pin];
       Room.time = Date.now();
       Room.started = true;
+      const quiz = Room.quiz;
       const index = Room.questionIndex;
+      const question = quiz.question[index];
 
-      quiz.find({ pin: pin }).then((result) => {
-        if (result.length != 0) {
-          var question = result[0].question[index];
-          Object.values(Room.players).forEach((player) => {
-            gameNamespace.to(player.socket.id).emit('render-content',
-              {
-                source: handleBarsSources["QH"],
-                templatedata: {
-                  questionImage: question.img,
-                  question: question.questionTitle,
-                  answer1: question.answers[0],
-                  answer2: question.answers[1],
-                  answer3: question.answers[2],
-                  answer4: question.answers[3],
-                  value: 100,
-                  time: question.time
-                }
-              });
+      Object.values(Room.players).forEach((player) => {
+        gameNamespace.to(player.socket.id).emit('render-content',
+          {
+            source: handleBarsSources["QH"],
+            templatedata: {
+              questionImage: question.img,
+              question: question.questionTitle,
+              answer1: question.answers[0],
+              answer2: question.answers[1],
+              answer3: question.answers[2],
+              answer4: question.answers[3],
+              value: 100,
+              time: question.time
+            }
           });
-          setTimeout(() => { statistics(pin); }, question.time * SECOND);
-        }
       });
+      setTimeout(() => { statistics(pin); }, question.time * SECOND);
     });
 
     function nextQ(pin) {
       var Room = Rooms[pin];
-      var players = Object.values(Room.players);
-      var index = ++(Room.questionIndex);
-      var results = [];
-
       Room.time = Date.now();
+      var players = Object.values(Room.players);
+      var quiz = Room.quiz;
+      var questions = quiz.question;
+      var index = ++(Room.questionIndex);
 
-      players.forEach((player) => {
-        results.push({ name: player.username, point: player.calculateTotalPoints() });
-      });
-      results.sort((a, b) => { return b.point - a.point; });
-      var firstPlace = results.splice(0, 1)[0];
-
-      if (index >= Room.questionCount) {
+      if (index >= questions.length) {
+        var results = [];
+        players.forEach((player) => {
+          results.push({ name: player.username, point: player.calculateTotalPoints() });
+        });
+        results.sort((a, b) => { return b.point - a.point; });
+        var firstPlace = results.splice(0, 1)[0];
         players.forEach((player) => {
           gameNamespace.to(player.socket.id).emit('render-content',
             {
@@ -154,114 +151,103 @@ module.exports = (io) => {
         delete Rooms[pin];
       }
       else {
-        quiz.find({ pin: pin }).then((result) => {
-          if (result.length != 0) {
-            var question = result[0].question[index];
-            players.forEach((player) => {
-              gameNamespace.to(player.socket.id).emit('render-content',
-                {
-                  source: handleBarsSources["QH"],
-                  templatedata: {
-                    questionImage: question.img,
-                    question: question.questionTitle,
-                    answer1: question.answers[0],
-                    answer2: question.answers[1],
-                    answer3: question.answers[2],
-                    answer4: question.answers[3],
-                    value: 100,
-                    time: question.time
-                  }
-                });
+        var question = questions[index];
+        players.forEach((player) => {
+          gameNamespace.to(player.socket.id).emit('render-content',
+            {
+              source: handleBarsSources["QH"],
+              templatedata: {
+                questionImage: question.img,
+                question: question.questionTitle,
+                answer1: question.answers[0],
+                answer2: question.answers[1],
+                answer3: question.answers[2],
+                answer4: question.answers[3],
+                value: 100,
+                time: question.time
+              }
             });
-            setTimeout(() => { statistics(pin); }, question.time * SECOND);
-          }
         });
+        setTimeout(() => { statistics(pin); }, question.time * SECOND);
       }
     }
 
     function statistics(pin) {
       var Room = Rooms[pin];
       var index = Room.questionIndex;
-      let players = Object.values(Room.players);
       var count = Room.playersAnswered;
-      quiz.find({ pin: pin }).then((result) => {
-        if (result.length != 0) {
-          var theQuiz = result[0];
-          var question = theQuiz.question[index];
-          var percentages = [
-            Room.answers[0] * 100 / count,
-            Room.answers[1] * 100 / count,
-            Room.answers[2] * 100 / count,
-            Room.answers[3] * 100 / count
-          ];
-          var colors = [
-            "lightgray",
-            "lightgray",
-            "lightgray",
-            "lightgray"
-          ];
-          colors[theQuiz.question[index].answer] = "green";
-          Room.playersAnswered = 0;
-          players.forEach((player) => {
-            gameNamespace.to(player.socket.id).emit('render-content',
-              {
-                source: handleBarsSources["SH"],
-                templatedata: {
-                  count: Room.answers,
-                  percent: percentages,
-                  color: colors,
-                  answer: question.answers
-                }
+      let players = Object.values(Room.players);
+      const quiz = Room.quiz;
+      const questions = quiz.question;
+      const question = questions[index];
+      const percentages = [
+        Room.answers[0] * 100 / count,
+        Room.answers[1] * 100 / count,
+        Room.answers[2] * 100 / count,
+        Room.answers[3] * 100 / count
+      ];
+      var colors = [
+        "lightgray",
+        "lightgray",
+        "lightgray",
+        "lightgray"
+      ]
 
-              });
+      colors[question.answer] = "green";
+      Room.playersAnswered = 0;
+
+      players.forEach((player) => {
+        gameNamespace.to(player.socket.id).emit('render-content',
+          {
+            source: handleBarsSources["SH"],
+            templatedata: {
+              count: Room.answers,
+              percent: percentages,
+              color: colors,
+              answer: question.answers
+            }
           });
-          Room.answers = [0, 0, 0, 0];
-          setTimeout(() => { nextQ(pin); }, STATISTICS_TIMEOUT);
-        }
       });
+
+      Room.answers = [0, 0, 0, 0];
+      setTimeout(() => { nextQ(pin); }, STATISTICS_TIMEOUT);
     }
 
     socket.on('give-answer', (data) => {
-      var pin = data.pin;
+      const pin = data.pin;
       var Room = Rooms[pin];
       var index = Room.questionIndex;
-      var thePlayer = Room.players[socket.id];
-      var value = data.val;
-      if ("isNull" in thePlayer.answers[index]) {
-        quiz.find({ pin: pin }).then((result) => {
-          if (result.length != 0) {
-            var theQuiz = result[0];
-            var question = theQuiz.question[index];
-            var answer = new Answer(Room.time, Date.now(), question.time * SECOND, data.answer, data.answer == question.answer);
-            thePlayer.answers[index] = answer;
-            Room.answers[data.answer]++;
-            Room.playersAnswered++;
-            var colors = [
-              "lightgray",
-              "lightgray",
-              "lightgray",
-              "lightgray"
-            ];
-            colors[data.answer] = "blue";
-            gameNamespace.to(socket.id).emit('render-content',
-              {
-                source: handleBarsSources["AH"],
-                templatedata: {
-                  questionImage: question.img,
-                  question: question.questionTitle,
-                  answer1: question.answers[0],
-                  answer2: question.answers[1],
-                  answer3: question.answers[2],
-                  answer4: question.answers[3],
-                  color1: colors[0],
-                  color2: colors[1],
-                  color3: colors[2],
-                  color4: colors[3]
-                }
-              });
+      const thePlayer = Room.players[socket.id];
+      const quiz = Room.quiz;
+      const question = quiz.question[index];
+      var answer = new Answer(Room.time, Date.now(), question.time * SECOND, data.answer, data.answer == question.answer);
+
+      thePlayer.answers[index] = answer;
+      Room.answers[data.answer]++;
+      Room.playersAnswered++;
+      var colors = [
+        "lightgray",
+        "lightgray",
+        "lightgray",
+        "lightgray"
+      ];
+      colors[data.answer] = "blue";
+      gameNamespace.to(socket.id).emit('render-content',
+        {
+          source: handleBarsSources["AH"],
+          templatedata: {
+            questionImage: question.img,
+            question: question.questionTitle,
+            answer1: question.answers[0],
+            answer2: question.answers[1],
+            answer3: question.answers[2],
+            answer4: question.answers[3],
+            color1: colors[0],
+            color2: colors[1],
+            color3: colors[2],
+            color4: colors[3]
           }
         });
-      }
     });
 
     socket.on('disconnect', () => {
